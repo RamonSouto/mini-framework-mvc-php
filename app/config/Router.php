1757f611b57840f0a3048b3e59e55d2c173cf092
RamonSouto <rdssi@hotmail.com>
2021-03-27 19:57:59 -0300

    Criando sistem de rota simples para chamar as rotas com funções

--- app/config/Router.php
+++ app/config/Router.php
@@ -1,2 +1,15 @@
 <?php
 
+$this->get('/', function () {
+    echo 'HOME';
+});
+
+$this->get('/home', function () {
+    echo 'Estou na HOME';
+});
+
+$this->get('/about/test', function () {
+    echo 'Estou na About Test :D';
+});
+
+$this->get('/categoria', 'MyController@method');
